79ce9558005da7f48c8142ad94d12b6c745a4e66
JSkierbi <jskierbi@gmail.com>
2013-02-10 01:03:38 +0100

    [QSyncVcxprj]
    + Added relative path generator for vcxproj

--- src/com/programmingteam/projects/QSyncVcxproj.java
+++ src/com/programmingteam/projects/QSyncVcxproj.java
@@ -3,10 +3,13 @@ package com.programmingteam.projects;
 import java.io.File;
 import java.util.ArrayList;
 
+import com.programmingteam.Helpers;
+
 public class QSyncVcxproj
 {
 	private String mVcxproj;
 	private String mVcxprojFilters;
+	private File mPwd;
 	private ArrayList<QSyncImport> mImportList;
 	
 	QSyncVcxproj(String proj, String filters)
@@ -14,6 +17,8 @@ public class QSyncVcxproj
 		mVcxproj = proj;
 		mVcxprojFilters = filters;
 		mImportList = new ArrayList<QSyncImport>();
+		
+		mPwd = (new File(mVcxproj)).getParentFile();
 	}
 	
 	void addImport(QSyncImport imp)
@@ -31,9 +36,32 @@ public class QSyncVcxproj
 		return mVcxprojFilters;
 	}
 	
-	File getPWD()
+	public File getPWD() 
 	{
-		return null;
+		return mPwd;
+	}
+	
+	/// \brief returns relative path between current pwd (vcxproj folder) and given file
+	/// Parameter file should be given as ABSOLUTE path
+	public File getRelativeFile(File inFile)
+	{
+		String input = inFile.getAbsolutePath();
+		String proj =  mVcxproj;
+		
+		int offset = 0;
+		for(; offset<input.length() && offset<proj.length(); ++offset)
+		{
+			if(input.charAt(offset)!=proj.charAt(offset))
+				break;
+		}
+		
+		StringBuffer buff = new StringBuffer();
+		for(int i=0; i<Helpers.countOccurances(proj.substring(offset, proj.length()), '\\'); ++i)
+		{
+			buff.append("..\\");
+		}
+		buff.append(input.substring(offset, input.length()));
+		return new File(buff.toString());
 	}
 	
 	public void debugPrint()
